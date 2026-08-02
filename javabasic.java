86b06b3194efb2dc38295bd09bc846bf0a24092e
nikhilspc <nssoni641@gmail.com>
2026-08-02 22:45:57 +0530

    next line use println

--- javabasic.java
+++ javabasic.java
@@ -1,5 +1,7 @@
 public class javabasic {
     public static void main(String[] args) {
-        System.out.println("1 2 3 4 5 6");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }

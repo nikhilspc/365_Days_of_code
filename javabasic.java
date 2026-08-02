534939877e7f1cfb902e3a8bc9534044073d40ef
nikhilspc <nssoni641@gmail.com>
2026-08-02 22:54:18 +0530

    simple pattern

--- javabasic.java
+++ javabasic.java
@@ -1,7 +1,9 @@
 public class javabasic {
     public static void main(String[] args) {
-        System.out.print("hello world\n");
-        System.out.print("hello world\n");
-        System.out.print("hello world\n");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }

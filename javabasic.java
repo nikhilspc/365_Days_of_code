0ce17c17f3854d5c5889e8502117d6b23366c79f
nikhilspc <nssoni641@gmail.com>
2026-08-02 22:48:53 +0530

    line change \n

--- javabasic.java
+++ javabasic.java
@@ -1,7 +1,7 @@
 public class javabasic {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.print("hello world\n");
+        System.out.print("hello world\n");
+        System.out.print("hello world\n");
     }
 }

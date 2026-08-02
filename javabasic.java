ecb5b92d6eedf0e020a275bd62de808005dbef7b
nikhilspc <nssoni641@gmail.com>
2026-08-02 23:02:30 +0530

    variables

--- javabasic.java
+++ javabasic.java
@@ -1,9 +1,8 @@
 public class javabasic {
     public static void main(String[] args) {
-        System.out.println("*****");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
     }
 }

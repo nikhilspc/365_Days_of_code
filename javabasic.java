142b9fc6d5ed3ecf071f44c858feed1f91931ae7
nikhilspc <nssoni641@gmail.com>
2026-08-03 21:16:15 +0530

    sum of two number

--- javabasic.java
+++ javabasic.java
@@ -2,7 +2,7 @@ public class javabasic {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println(a);
-        System.out.println(b);
+        int sum = a + b;
+        System.out.println("The sum of a and b is: " + sum);
     }
 }

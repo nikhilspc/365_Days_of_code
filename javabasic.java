35f93e0b849096f10611121d513815a3d8fb735a
nikhilspc <nssoni641@gmail.com>
2026-08-05 22:19:13 +0530

    compare number

--- javabasic.java
+++ javabasic.java
@@ -3,7 +3,16 @@ public class javabasic {
         int a = 10;
         int b = 20;
         int sum = a + b;
-        System.out.println("The sum of a and b is: " + sum);
-        
+        compareNumbers(a, b);
+    }
+
+    public static void compareNumbers(int x, int y) {
+        if (x > y) {
+            System.out.println(x + " is greater than " + y);
+        } else if (y > x) {
+            System.out.println(y + " is greater than " + x);
+        } else {
+            System.out.println("Both numbers are equal");
+        }
     }
 }

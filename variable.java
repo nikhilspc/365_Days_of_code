7c08ce8c2682c2a65bf4204af8e9ca6a23da107b
nikhilspc <nssoni641@gmail.com>
2026-08-11 22:23:49 +0530

    variables

---
+++ variable.java
@@ -0,0 +1,20 @@
+class Variable {
+    public static void main(String[] args) {
+        
+        // Declaring and initializing variables
+        
+        // Integer variable  
+        int age = 25;       
+        
+        // String variable  
+        String name = "nikhil"; 
+        
+        // Double variable 
+        double salary = 50000.50;     
+
+        // Displaying the values of variables
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+    }
+}
